a04a161d8832dc454e0efb0f2b49f8b1fd49016a
Josh S <spajos07@gmail.com>
2025-06-29 22:13:57 +0200

    V1.3 Way cleaner SocketIO Server code

--- routes/socket.js
+++ routes/socket.js
@@ -4,59 +4,32 @@ const { cache } = require("../data/cache");
 function routeSocket(app, io) {
     io.on("connection", (socket) => {
         log("Connection");
-        assignRoom();
 
-        // middleware for cookie parsing etc
+        const cookies = cookie.parse(socket.handshake.headers.cookie);
+        let game = cache.games.find((game) => game.getPlayer(cookies.sid));
+        if (!game) return;
+        let player = game.getPlayer(cookies.sid);
+        if (!player) return;
 
-        socket.on("disconnect", () => {
-            log("Disconnection");
-        });
+        socket.join(game.gid);
+        io.to(game.gid).emit("refresh", true);
 
-        socket.on("leave", () => {
-            const cookies = cookie.parse(socket.handshake.headers.cookie);
-
-            let game = cache.games.find((game) => game.getPlayer(cookies.sid));
-            if (!game) return;
-
-            io.to(game.gid).emit("refresh", true);
-        });
-
-        socket.on("move", move);
-
-        /**
-         * @param data
-         * @param {String} data.move
-         */
-        function move(data) {
-            const cookies = cookie.parse(socket.handshake.headers.cookie);
-
-            let game = cache.games.find((game) => game.getPlayer(cookies.sid));
-            if (!game) return;
-
-            // Check if req is player
-            let player = game.getPlayer(cookies.sid);
-            if (!player) return;
-
+        socket.on("move", (data) => {
             logMsg(player, "Move", data.move);
 
             game.move(data.move, player);
 
             io.to(game.gid).emit("refresh", true);
-        }
+        });
 
-        function assignRoom() {
-            var cookies = cookie.parse(socket.handshake.headers.cookie);
-
-            let game = cache.games.find((game) => game.getPlayer(cookies.sid));
-            if (!game) return;
-
-            // Check if req is player
-            let player = game.getPlayer(cookies.sid);
-            if (!player) return;
-
-            socket.join(game.gid);
+        socket.on("leave", () => {
+            logMsg(player, "Leave");
             io.to(game.gid).emit("refresh", true);
-        }
+        });
+
+        socket.on("disconnect", () => {
+            log("Disconnection");
+        });
     });
 }
 
